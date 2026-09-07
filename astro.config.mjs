bb4a005ac2678bba217d5ea7dd05c5b191cae2de
Dusan Cajic <dusancajic89@gmail.com>
2026-09-07 20:37:56 +0200

    config file updated.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,5 @@ const isGithub = process.env.GITHUB_ACTIONS === 'true';
 
 export default defineConfig({
   site: isGithub ? 'https://DC989.github.io' : 'http://localhost:4322',
-  base: isGithub ? '/svecicezaauto' : '/',
+  base: isGithub ? '/svecicezaauto/' : '/',
 });
